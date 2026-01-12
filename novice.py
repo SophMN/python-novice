fe59e69feda923df477cf35b7a768e5bff5ae03c
Sophia Mogere <mogeresophia@gmail.com>
2026-01-12 11:57:51 +0300

    intro to pandas

---
+++ novice.py
@@ -0,0 +1,22 @@
+##Creating, reading and writing
+
+#Import pandas library
+import pandas as pd
+
+#Create a pandas DataFrame
+pd.DataFrame({'Yes': [20, 21], 'No': [130, 3]})
+
+pd.DataFrame({'Bob': ['Disagree', 'Neutral'], 
+              'Sue': ['Strongly agree', 'Agree']})
+
+#Adding row labels using the index parameter
+pd.DataFrame({'Bob': ['Disagree', 'Neutral'], 
+              'Sue': ['Strongly agree', 'Agree']}, 
+              index = ['Question1', 'Question2'])
+
+#Pandas Series
+pd.Series([30, 35, 40], index = ['2015 sales', '2016 sales', '2017 sales'], 
+name = 'ProductA')
+
+#Reading data
+wine_reviews = pd.read_csv()
